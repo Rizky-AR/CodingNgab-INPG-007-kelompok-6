7acb22973179aeac804a96b90b732534cd8bb823
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2022-11-04 21:58:08 +0800

    Memperbaiki struktur html

--- js/script.js
+++ js/script.js
@@ -3,23 +3,22 @@ document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
     document.querySelector('.subscription').classList.add('done');
   });
   
-  // variabel
-  const waktu_expired = 3 * 60 * 1000; // 3 menit
-  const login_btn = document.querySelector('#login-btn');
-  const signup_btn = document.querySelector('#signup-btn');
-  const logout_btn = document.querySelector('#logout-btn');
-  const profile_btn = document.querySelector('#profile-btn');
-  const btn_close_login = document.querySelector('#btn_close_login');
-  const login_section = document.querySelector('#login_section');
-  const bg_login_register = document.querySelector('#bg_login_register');
-  const html = document.querySelector('html');
+const waktu_expired = 3 * 60 * 1000; // 3 menit
+const login_btn = document.querySelector('#login-btn');
+const signup_btn = document.querySelector('#signup-btn');
+const logout_btn = document.querySelector('#logout-btn');
+const profile_btn = document.querySelector('#profile-btn');
+const btn_close_login = document.querySelector('#btn_close_login');
+const btn_close_register = document.querySelector('#btn_close_register');
+const login_section = document.querySelector('#login_section');
+const register_section = document.querySelector('#register_section');
+const bg_login_register = document.querySelector('#bg_login_register');
+const html = document.querySelector('html');
   
-  // login
-  const login_form = document.querySelector("#form_login");
-  const login_email = document.querySelector("#login_email");
-  const login_password = document.querySelector("#login_password");
+const login_form = document.querySelector("#form_login");
+const login_email = document.querySelector("#login_email");
+const login_password = document.querySelector("#login_password");
 
-  // registrasi
 const register_form = document.querySelector("#form_register");
 const register_username = document.querySelector("#register_username");
 const register_email = document.querySelector("#register_email");
@@ -27,8 +26,7 @@ const register_password = document.querySelector("#register_password");
 const register_password_konfirmasi = document.querySelector("#register_password_konfirmasi");
   
   
-  // action
-  function checkLogin() {
+function checkLogin() {
     const dt_login = JSON.parse(localStorage.getItem('login'));
     const session_login = sessionStorage.getItem('login');
     if(session_login == null) {
@@ -43,7 +41,6 @@ const register_password_konfirmasi = document.querySelector("#register_password_
         logout();
         return false;
     } else {
-        // perbaharui last login nya
         const data_login = {
             status: true,
             last_login: new Date().getTime(),
@@ -54,7 +51,6 @@ const register_password_konfirmasi = document.querySelector("#register_password_
   }
   
   window.addEventListener("load", (e) => {
-    // check login atau blm
     const isLogin = checkLogin();
     if(isLogin == true) {
         showNavLogin(true);
@@ -127,10 +123,7 @@ btn_close_register.addEventListener('click', (e) => {
     showRegisterSection(false);
 });
 
-// function
 function validasiRegister(username, email, password, password_konfirmasi) {
-    // validasi username
-    // check panjang karakter
     if (username.length < 5 || username.length > 10) {
       return showValidationError("Panjang username harus 5-10 karakter");
     }
@@ -140,13 +133,10 @@ function validasiRegister(username, email, password, password_konfirmasi) {
     for (i = 0; i < username.length; i++) {
       let code = username.charCodeAt(i);
       if (code > 47 && code < 58) {
-        // check number 0-9
         usernameContainsNumber = true;
       } else if ((code > 64 && code < 91) || (code > 96 && code < 123)) {
-        // check alfabet a-z dan A-Z
         usernameContainsAlfabet = true;
       } else {
-        // check spasi dan karakter lain
         usernameContainsOther = true;
       }
     }
@@ -159,15 +149,12 @@ function validasiRegister(username, email, password, password_konfirmasi) {
       return showValidationError("Username harus ada huruf dan angka");
     }
 
-    // validasi email
-    var karakterAt=email.indexOf("@");
+  var karakterAt=email.indexOf("@");
 	var karakterTitik=email.indexOf(".");
 	if (karakterAt < 1 || karakterTitik < 1) {
         return showValidationError("Email Tidak valid");
     }
   
-    // validasi password
-    // check panjang karakter
     if (password.length < 6 || password.length > 12) {
       return showValidationError("Panjang password harus 6-12 karakter");
     }
@@ -178,16 +165,12 @@ function validasiRegister(username, email, password, password_konfirmasi) {
     for (i = 0; i < password.length; i++) {
       let code = password.charCodeAt(i);
       if (code > 47 && code < 58) {
-        // check number 0-9
         passwordContainsNumber = true;
       } else if (code > 96 && code < 123) {
-        // check alfabet a-z
         passwordContainsAlfabet = true;
       } else if (code > 64 && code < 91) {
-        // check alfabet contains one capital
         passwordCapitalAflabet = passwordCapitalAflabet + 1;
       } else {
-        // check spasi dan karakter lain
         passwordContainsOther = true;
       }
     }
@@ -201,8 +184,6 @@ function validasiRegister(username, email, password, password_konfirmasi) {
         "password harus ada huruf, angka dan satu huruf kapital"
       );
     }
-  
-    //   validasi konfirmasi password
     if (password_konfirmasi != password) {
       return showValidationError("Konfirmasi Password tidak sesuai");
     }
@@ -264,9 +245,11 @@ function showRegisterSection(show) {
     if(show == true) {
         bg_login_register.classList.add('active');
         register_section.classList.add('active');
-    } else {
+        html.classList.add('hidden-ovrflw')
+      } else {
         bg_login_register.classList.remove('active');
         register_section.classList.remove('active');
+        html.classList.remove('hidden-ovrflw')
     }
 }
 
@@ -288,11 +271,9 @@ function showNavLogin(isLogin = false) {
         logout_btn.classList.add('d-none');
         profile_btn.classList.add('d-none');
     } else {
-        // ambil username
         const dt_login = JSON.parse(localStorage.getItem('account'));
         profile_btn.innerHTML = '';
         profile_btn.innerHTML = dt_login.username;
-
         login_btn.classList.add('d-none');
         signup_btn.classList.add('d-none');
         logout_btn.classList.remove('d-none');
