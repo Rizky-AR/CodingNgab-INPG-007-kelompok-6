cc6ab25d0486d1a3a8023ee00560132cc96fbe34
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2022-11-05 08:47:32 +0800

    Menambahkan back to top button

--- js/script.js
+++ js/script.js
@@ -308,3 +308,17 @@ let setDarkMode = localStorage.getItem('dark');
 if(setDarkMode === 'on'){
   darkmode();
 }
+
+let topButton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
